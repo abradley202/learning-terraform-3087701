2448da56abd52d03e27772b9da1375685f11e7c8
abradley202 <105672783+abradley202@users.noreply.github.com>
2025-01-21 13:41:40 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-
+}
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -61,26 +61,6 @@ module "alb" {
   }
 
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
 
   target_groups = {
     ex-instance = {
