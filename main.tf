f85590668b07b1c5a2dddb1fd946b2612dfdbe91
abradley202 <105672783+abradley202@users.noreply.github.com>
2025-01-21 13:09:40 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog_alb"
-  load_balancer_type = "application'
+  load_balancer_type = "application"
 
   vpc_id          = "module.blog_vpc.vpc_id"
   subnets         = module.blog_vpc.public_subnets
