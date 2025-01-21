6fe274913cde6416d2c0d380d47c28c4208d8bf2
abradley202 <105672783+abradley202@users.noreply.github.com>
2025-01-21 13:39:47 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,20 +58,6 @@ module "alb" {
       ip_protocol = "tcp"
       description = "HTTP web traffic"
       cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
   }
 
 
