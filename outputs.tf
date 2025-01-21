626d66e03155ea68844df4c166cfa1a835f01c88
abradley202 <105672783+abradley202@users.noreply.github.com>
2025-01-21 13:24:31 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blog.id
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog.id
+  value = aws_instance.blog.arn
 }
